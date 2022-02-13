e5b8ab2f1a75958381badd4b24e20cc1dd3913f1
Gowthamram983 <32929518+Gowthamram983@users.noreply.github.com>
2022-02-13 16:17:54 +0000

    Update format.cpp

--- CppND-System-Monitor/src/format.cpp
+++ CppND-System-Monitor/src/format.cpp
@@ -3,18 +3,25 @@
 #include <string>
 
 using std::string;
+using std::to_string;
 
+// To format the time  
+string Format::TimeFormat(int t) {
+  if (to_string(t).size() < 2) {
+    return '0' + to_string(t);
+  } else {
+    return to_string(t);
+  }
+}
 // TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
 string Format::ElapsedTime(long seconds) {
-  long hour = seconds / 3600;
-  long minute = seconds / 60;
-  long second = seconds; // % 60;
-  std::string HH = std::to_string(hour);
-  std::string MM = std::to_string(minute % 60); //ensure its does not display value > 60
-  std::string SS = std::to_string(second % 60); //ensure its does not display value > 60
-  std::string OUTPUT = HH + ":" + MM + ":" + SS;
-  return OUTPUT;
+    int hh, mm, ss;
+  string time;
+  hh = seconds / 3600;
+  mm = (seconds % 3600) / 60;
+  ss = (seconds % 3600) % 60;
+  return TimeFormat(hh) + ':' + TimeFormat(mm) + ':' + TimeFormat(ss);
 }
